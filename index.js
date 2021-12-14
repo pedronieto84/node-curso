4dd869f245a2f0d0d2b3fa7e0a5ac52be7ab1d47
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-12-14 23:34:41 +0100

    Get, Post, Put, Delete

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/get", (req, res) => {
+  res.send("Això es un GET");
+});
+
+app.post("/post", (req, res) => {
+  res.send("Això es un POST");
+});
+
+app.put("/put", (req, res) => {
+  res.send("Això es un PUT");
+});
+
+app.delete("/delete", (req, res) => {
+  res.send("Això es un delete");
 });
 
 app.listen(port, () => {
